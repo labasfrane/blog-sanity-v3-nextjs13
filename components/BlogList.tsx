ec832877b007d721216f0e016acecb941a2f4c84
Frane <labasfrane@gmail.com>
2023-02-27 18:30:04 +0100

    fix

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -16,20 +16,23 @@ const BlogList = ({ posts }: Props) => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
-          <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+          <ClientSideRoute
+            key={post?._id}
+            route={`/post/${post?.slug?.current}`}
+          >
             <div className="flex flex-col group cursor-pointer">
               <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
                 <Image
                   className="object-cover object-left lg:object-center"
-                  src={urlFor(post.mainImage).url()}
-                  alt={post.author.name}
+                  src={urlFor(post?.mainImage)?.url()}
+                  alt={post?.author?.name}
                   fill
                 />
                 <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
                   <div>
-                    <p>{post.title}</p>
+                    <p>{post?.title}</p>
                     <p>
-                      {new Date(post._createdAt).toLocaleDateString("en-US", {
+                      {new Date(post?._createdAt).toLocaleDateString("en-US", {
                         day: "numeric",
                         month: "long",
                         year: "numeric",
@@ -38,7 +41,7 @@ const BlogList = ({ posts }: Props) => {
                   </div>
 
                   <div className="flex flex-col md:flex-row gap-y-4 md:gap-x-4 items-center">
-                    {post.categories.map((category) => (
+                    {post?.categories?.map((category) => (
                       <div
                         key={category._id}
                         className="bg-slate-400 text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
@@ -50,8 +53,10 @@ const BlogList = ({ posts }: Props) => {
                 </div>
               </div>
               <div className="mt-5 flex-1">
-                <p className="underline text-lg font-bold">{post.title}</p>
-                <p className="line-clamp-2 text-gray-500">{post.description}</p>
+                <p className="underline text-lg font-bold">{post?.title}</p>
+                <p className="line-clamp-2 text-gray-500">
+                  {post?.description}
+                </p>
               </div>
 
               <p className="flex font-bold">
