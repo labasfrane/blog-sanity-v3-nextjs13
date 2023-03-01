3f34661c32ac963e60299d67134290b407371838
Frane <labasfrane@gmail.com>
2023-03-01 11:43:12 +0100

    added meta data

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,16 @@ export default function Head() {
     <>
       <title>HDEPO</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="keywords"
+        content="hrvatsko, društvo, embriologija, potpomognuta, oplodnja"
+      ></meta>
+      <meta
+        name="description"
+        content="Hrvatsko društvo za embriologiju i potpomognutu oplodnju"
+      />
       <link rel="icon" href="/favicon1.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
     </>
   );
 }
