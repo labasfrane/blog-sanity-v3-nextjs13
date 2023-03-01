c9854e165e5517032354de5299dfb6c6309e5d80
Frane <labasfrane@gmail.com>
2023-03-01 15:35:08 +0100

    update

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -34,8 +34,8 @@ async function Post({ params: { slug } }: Props) {
   const post: Post = await client.fetch(query, { slug });
 
   return (
-    <article className="pb-20 mx-5 mt-1">
-      <section className="space-y-2 border-t border-neutral-400">
+    <article className="pb-20 mt-1">
+      <section className="space-y-2 px-2 border-t border-b border-neutral-400 mb-10">
         <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
           <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
             <Image
@@ -45,19 +45,27 @@ async function Post({ params: { slug } }: Props) {
               fill
             />
           </div>
+
+          <section className="p-5 w-full bg-neutral-100">
+            <div className="flex flex-col md:flex-row justify-between gap-y-5">
+              <div>
+                <h1 className="text-4xl font-extrabold">{post?.title}</h1>
+                <h2 className="italic pt-2 pb-6">{post.description}</h2>
+                <p className="ml-auto">
+                  {new Date(post?._createdAt).toLocaleDateString("hr-HR", {
+                    day: "2-digit",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
-
-      <section className="p-5 w-full">
-        <div className="flex flex-col md:flex-row justify-between gap-y-5">
-          <div>
-            <h1>{post?.title}</h1>
-            <p></p>
-          </div>
-        </div>
+      <section className="mx-8">
+        <PortableText value={post?.body} components={RichTextComponents} />
       </section>
-
-      <PortableText value={post?.body} components={RichTextComponents} />
     </article>
   );
 }
